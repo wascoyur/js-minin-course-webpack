5fbf83cc3e632e3062835e74f6201eb118d1a21f
wascoyur <mytem20007@gmail.com>
2020-05-24 18:01:34 +0500

    добавлены стили работают

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import Post from './Post'
+import './styles/style.css'
 const post = new Post('Webpack Ost Title');
 console.log(post.toString());
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-	mode:"development",
+	context: path.resolve(__dirname,'src'),
 	entry: {
-		main: './src/index.js',
-		analitic: './src/analitic.js'
+		main: './index.js',
+		analitic: './analitic.js'
 	},
 	output: {
 		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	plugins: [
-		new HtmlWebPackPlugin()
-	]
+		new HtmlWebPackPlugin({
+			template: './index.html'
+		}),
+		new CleanWebpackPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /.css$/,
+				use:['style-loader','css-loader']
+			}
+		]
+	}
 }
