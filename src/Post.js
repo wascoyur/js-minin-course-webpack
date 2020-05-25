c0406c648ee7b6214c3e01f90cd811518db0eda5
wascoyur <mytem20007@gmail.com>
2020-05-25 15:32:44 +0500

    повторно добавлены данные

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,8 @@
 export default class Post {
-	constructor(title) {
+	constructor(title, img) {
 		this.title = title;
 		this.date = new Date();
+		this.img = img;
 	}
 	toString() {
 		return JSON.stringify({
